a14058a3487b170637e736f57f7ad19fdfb67788
gim34 <gim34@naver.com>
2020-09-13 21:03:39 +0900

    Add post feature

--- src/api/posts/posts.controller.ts
+++ src/api/posts/posts.controller.ts
@@ -1,19 +1,57 @@
-import { Controller, Get, Param, HttpException, HttpStatus, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  HttpException,
+  HttpStatus,
+  Body,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsEntity } from '@/models/entities';
 
 @Controller('/posts')
 export class PostsController {
-  constructor(private readonly PostsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get('')
+  public async getPostList(): Promise<Array<PostsEntity>> {
+    let postList: Array<PostsEntity>;
+    try {
+      postList = await this.postsService.getPostList();
+    } catch {
+      throw new HttpException(`Can't get post list`, HttpStatus.NOT_FOUND);
+    }
+    return postList;
+  }
 
   @Get(':postId')
   public async getPost(@Param('postId') postId: number): Promise<PostsEntity> {
     let post: PostsEntity;
     try {
-        post = await this.PostsService.getPost(postId);
+      post = await this.postsService.getPost(postId);
     } catch (error) {
-        throw new HttpException(`Not found post by ID: ${ postId }`, HttpStatus.NOT_FOUND);
+      throw new HttpException(
+        `Not found post by ID: ${postId}`,
+        HttpStatus.NOT_FOUND,
+      );
     }
     return post;
+  }
+
+  @Post('')
+  public async addPost(@Body() post: PostsEntity) {
+    console.log(post);
+    try {
+      await this.postsService.addPost(post);
+    } catch {
+      throw new HttpException(`Can't insert post`, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @Get('/test/test')
+  public async test(): Promise<PostsEntity> {
+    const test = await this.postsService.test();
+    return test;
   }
 }
--- src/api/posts/posts.service.ts
+++ src/api/posts/posts.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { PostsEntity } from '@/models/entities/';
+import { PostsEntity, UserEntity } from '@/models/entities/';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    @InjectRepository(PostsEntity)
+    private readonly postsRepository: Repository<PostsEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-    constructor(@InjectRepository(PostsEntity) private readonly postsRepository: Repository<PostsEntity>) {}
+  public async getPostList(): Promise<Array<PostsEntity>> {
+    const postList = await this.postsRepository.find();
+    return postList;
+  }
 
-    public async getPost(postId: number): Promise<PostsEntity> {
-        const post = await this.postsRepository.findOneOrFail({
-            where: { id: postId },
-        });
-        return post;
-    }
+  public async getPost(postId: number): Promise<PostsEntity> {
+    const post = await this.postsRepository.findOneOrFail({
+      where: { id: postId },
+    });
+    return post;
+  }
+
+  public async addPost(post: PostsEntity) {
+    await this.postsRepository.insert(post);
+  }
+
+  public async test(): Promise<PostsEntity> {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id: 1,
+      },
+    });
+    const user = await this.userRepository.findOne({
+      where: {
+        id: post.userId,
+      },
+    });
+    post.user = user;
+    console.log(post);
+    return post;
+  }
 }
